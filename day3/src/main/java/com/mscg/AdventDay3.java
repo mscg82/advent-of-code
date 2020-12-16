1b6c1027defd02a07ae37c4bbec33799a5096af9
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2020-12-16 19:00:03 +0100

    Day 3

---
+++ day3/src/main/java/com/mscg/AdventDay3.java
@@ -0,0 +1,16 @@
+package com.mscg;
+
+public class AdventDay3 {
+
+    public static void main(String[] args) throws Exception {
+        part1();
+        part2();
+    }
+
+    private static void part1() throws Exception {
+    }
+
+    private static void part2() throws Exception {
+    }
+
+}
