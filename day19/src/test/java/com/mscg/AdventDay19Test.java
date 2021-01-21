6c3030f47e7e76af4f793f91d4fc481488a6b22e
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-01-21 16:02:25 +0100

    day 19 - refactor

--- day19/src/test/java/com/mscg/AdventDay19Test.java
+++ day19/src/test/java/com/mscg/AdventDay19Test.java
@@ -3,7 +3,6 @@ package com.mscg;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Assertions;
