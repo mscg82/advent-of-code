c772a0246c7ee46d8bbee9ada8277be4be9b8f95
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-02-02 18:42:40 +0100

    day 23 - stub

---
+++ day23/src/main/java/com/mscg/AdventDay23.java
@@ -0,0 +1,36 @@
+package com.mscg;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class AdventDay23 {
+
+    public static void main(String[] args) throws Exception {
+        part1();
+        part2();
+    }
+
+    private static void part1() throws Exception {
+        try (BufferedReader in = readInput()) {
+            // var game = CombatGame.parseInput(readInput());
+            // System.out.println("Part 1: Answer: %d".formatted(game.getGameValue()));
+        }
+    }
+
+    private static void part2() throws Exception {
+        try (BufferedReader in = readInput()) {
+            // var game = CombatGame.parseInput(readInput());
+
+            // GameStatus status = game.playRecursiveGame();
+
+            // System.out.println("Part 2: Answer: %d".formatted(game.getValidationNumber(status)));
+        }
+    }
+
+    private static BufferedReader readInput() {
+        return new BufferedReader(
+                new InputStreamReader(AdventDay23.class.getResourceAsStream("/input.txt"), StandardCharsets.UTF_8));
+    }
+
+}
