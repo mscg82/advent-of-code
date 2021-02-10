ab3465ca3f6866d194973986db79265d38ad31cd
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-02-10 18:00:30 +0100

    day 1 - stub

---
+++ day1/src/main/java/com/mscg/AdventDay1.java
@@ -0,0 +1,28 @@
+package com.mscg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class AdventDay1 {
+
+    public static void main(String[] args) throws Exception {
+        part1();
+        part2();
+    }
+
+    private static void part1() throws IOException {
+        
+    }
+
+    private static void part2() throws IOException {
+        
+    }
+
+    private static BufferedReader readInput() {
+        return new BufferedReader(
+                new InputStreamReader(AdventDay1.class.getResourceAsStream("/input.txt"), StandardCharsets.UTF_8));
+    }
+
+}
