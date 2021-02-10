9d154ab94eca6d2534be575a313142d2f7e67afd
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-02-10 18:30:49 +0100

    day 1 - part 1

--- day1/src/main/java/com/mscg/AdventDay1.java
+++ day1/src/main/java/com/mscg/AdventDay1.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdventDay1 {
 
@@ -13,11 +15,23 @@ public class AdventDay1 {
     }
 
     private static void part1() throws IOException {
-        
+        try (BufferedReader in = readInput()) {
+            var line = in.readLine();
+            Map<Character, Long> counts = line.chars() //
+                    .collect(() -> new HashMap<Character, Long>(), //
+                            (acc, v) -> {
+                                acc.merge((char) v, 1L, Long::sum);
+                            }, //
+                            (m1, m2) -> {
+                                m2.forEach((k, v) -> m1.merge(k, v, Long::sum));
+                            });
+            long floor = counts.get('(') - counts.get(')');
+            System.out.println("Part 1 - Answer %d".formatted(floor));
+        }
     }
 
     private static void part2() throws IOException {
-        
+
     }
 
     private static BufferedReader readInput() {
