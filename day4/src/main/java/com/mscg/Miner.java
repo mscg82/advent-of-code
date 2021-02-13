061c13a670a621f8733e5f7336e06f26e1458a16
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-02-13 10:43:39 +0100

    day 4 - functional solution

--- day4/src/main/java/com/mscg/Miner.java
+++ day4/src/main/java/com/mscg/Miner.java
@@ -1,5 +1,7 @@
 package com.mscg;
 
+import java.util.stream.IntStream;
+
 import org.apache.commons.codec.digest.DigestUtils;
 
 import lombok.RequiredArgsConstructor;
@@ -10,24 +12,36 @@ public class Miner {
     private final String seed;
 
     public int mine() {
-        for (int i = 1; i < 100_000_000; i++) {
-            String hash = DigestUtils.md5Hex(seed + i);
-            if ("00000".equals(hash.substring(0, 5))) {
-                return i;
-            }
+        long before = System.nanoTime();
+        try {
+            return IntStream.range(0, 100_000_000) //
+                    // .parallel() //
+                    .filter(i -> {
+                        String hash = DigestUtils.md5Hex(seed + i);
+                        return "00000".equals(hash.substring(0, 5));
+                    }) //
+                    .findFirst() //
+                    .orElseThrow(() -> new IllegalStateException("Unable to mine in 100.000.000 steps"));
+        } finally {
+            long elapsed = System.nanoTime() - before;
+            System.out.println("mine() took %.3f ms".formatted(elapsed / 1_000_000.0));
         }
-
-        throw new IllegalStateException("Unable to mine in 100.000.000 steps");
     }
 
     public int mine2() {
-        for (int i = 1; i < 100_000_000; i++) {
-            String hash = DigestUtils.md5Hex(seed + i);
-            if ("000000".equals(hash.substring(0, 6))) {
-                return i;
-            }
+        long before = System.nanoTime();
+        try {
+            return IntStream.range(0, 100_000_000) //
+                    // .parallel() //
+                    .filter(i -> {
+                        String hash = DigestUtils.md5Hex(seed + i);
+                        return "000000".equals(hash.substring(0, 6));
+                    }) //
+                    .findFirst() //
+                    .orElseThrow(() -> new IllegalStateException("Unable to mine in 100.000.000 steps"));
+        } finally {
+            long elapsed = System.nanoTime() - before;
+            System.out.println("mine2() took %.3f ms".formatted(elapsed / 1_000_000.0));
         }
-
-        throw new IllegalStateException("Unable to mine in 100.000.000 steps");
     }
 }
