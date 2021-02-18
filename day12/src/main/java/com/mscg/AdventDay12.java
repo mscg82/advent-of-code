18eab4bd355e93c67eae11c0d4117cb50311dd66
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-02-18 15:08:04 +0100

    day 12 - part 1

--- day12/src/main/java/com/mscg/AdventDay12.java
+++ day12/src/main/java/com/mscg/AdventDay12.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class AdventDay12 {
 
@@ -14,18 +16,26 @@ public class AdventDay12 {
 
     private static void part1() throws IOException {
         try (BufferedReader in = readInput()) {
-            // long niceStrings = in.lines() //
-            //         .filter(StringFilter::isNice) //
-            //         .count();
-            // System.out.println("Part 1 - Answer %d".formatted(niceStrings));
+            String input = in.lines() //
+                    .collect(Collectors.joining());
+            var pattern = Pattern.compile("(-?\\d+)");
+            var matcher = pattern.matcher(input);
+
+            int sum = 0;
+            while (matcher.find()) {
+                int val = Integer.parseInt(matcher.group(1));
+                sum += val;
+            }
+
+            System.out.println("Part 1 - Answer %d".formatted(sum));
         }
     }
 
     private static void part2() throws IOException {
         try (BufferedReader in = readInput()) {
             // long niceStrings = in.lines() //
-            //         .filter(StringFilter::isNice2) //
-            //         .count();
+            // .filter(StringFilter::isNice2) //
+            // .count();
             // System.out.println("Part 2 - Answer %d".formatted(niceStrings));
         }
     }
