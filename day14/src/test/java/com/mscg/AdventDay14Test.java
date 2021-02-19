2485cd1e5aad235bf78e997409eb647ef852682d
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-02-19 17:54:21 +0100

    day 14 - stub

---
+++ day14/src/test/java/com/mscg/AdventDay14Test.java
@@ -0,0 +1,14 @@
+package com.mscg;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class AdventDay14Test {
+
+    private BufferedReader readInput() {
+        return new BufferedReader(
+                new InputStreamReader(this.getClass().getResourceAsStream("/test-input.txt"), StandardCharsets.UTF_8));
+    }
+
+}
