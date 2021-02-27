fe25a559f8c5cec6eb1cd07d6a427817fff22d2a
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-02-27 21:02:32 +0100

    day 20 - stub

---
+++ day20/src/main/java/com/mscg/AdventDay20.java
@@ -0,0 +1,38 @@
+package com.mscg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class AdventDay20 {
+
+    public static void main(String[] args) throws Exception {
+        part1();
+        part2();
+    }
+
+    private static void part1() throws IOException {
+        try (BufferedReader in = readInput()) {
+            // String input = in.lines() //
+            //         .collect(Collectors.joining());
+
+            // System.out.println("Part 1 - Answer %d".formatted(JsonCleaner.sumValues(input)));
+        }
+    }
+
+    private static void part2() throws Exception {
+        try (BufferedReader in = readInput()) {
+            // String input = in.lines() //
+            //         .collect(Collectors.joining());
+
+            // System.out.println("Part 2 - Answer %d".formatted(JsonCleaner.sumValues(JsonCleaner.cleanJson(input))));
+        }
+    }
+
+    private static BufferedReader readInput() {
+        return new BufferedReader(
+                new InputStreamReader(AdventDay20.class.getResourceAsStream("/input.txt"), StandardCharsets.UTF_8));
+    }
+
+}
