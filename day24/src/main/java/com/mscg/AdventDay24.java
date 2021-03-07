725bd00d1adc0ffa89856e70c4b13fff982a6e36
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-03-07 18:04:15 +0100

    day 24 - stub

---
+++ day24/src/main/java/com/mscg/AdventDay24.java
@@ -0,0 +1,36 @@
+package com.mscg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class AdventDay24 {
+
+    public static void main(String[] args) throws Exception {
+        part1();
+        part2();
+    }
+
+    private static void part1() throws IOException {
+        try (BufferedReader in = readInput()) {
+            // var containers = ContainerList.parseInput(in);
+
+            // System.out.println("Part 1 - Answer %d".formatted(containers.computeCombinations()));
+        }
+    }
+
+    private static void part2() throws Exception {
+        try (BufferedReader in = readInput()) {
+            // var containers = ContainerList.parseInput(in);
+
+            // System.out.println("Part 2 - Answer %d".formatted(containers.computeShorterCombinations()));
+        }
+    }
+
+    private static BufferedReader readInput() {
+        return new BufferedReader(
+                new InputStreamReader(AdventDay24.class.getResourceAsStream("/input.txt"), StandardCharsets.UTF_8));
+    }
+
+}
