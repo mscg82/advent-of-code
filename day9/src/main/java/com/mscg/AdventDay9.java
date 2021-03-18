72b4342103a82703629d9fc1b96bc3b508206a32
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-03-18 11:03:47 +0100

    day 9 - stub

---
+++ day9/src/main/java/com/mscg/AdventDay9.java
@@ -0,0 +1,32 @@
+package com.mscg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class AdventDay9 {
+    
+    public static void main(String[] args) throws Exception {
+        part1();
+        part2();
+    }
+
+    private static void part1() throws IOException {
+        try (BufferedReader in = readInput()) {
+            System.out.println("Part 1 - Answer %d".formatted(0));
+        }
+    }
+
+    private static void part2() throws IOException {
+        try (BufferedReader in = readInput()) {
+            System.out.println("Part 2 - Answer %d".formatted(0));
+        }
+    }
+
+    private static BufferedReader readInput() {
+        return new BufferedReader(
+                new InputStreamReader(AdventDay9.class.getResourceAsStream("/input.txt"), StandardCharsets.UTF_8));
+    }
+
+}
