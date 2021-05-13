1b86c85d7bd7fce6a215fb8f812b0f5f06da607a
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2021-05-13 19:29:55 +0200

    day 3 - part 2

--- day3/src/main/java/com/mscg/AdventDay3.java
+++ day3/src/main/java/com/mscg/AdventDay3.java
@@ -6,22 +6,23 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 public class AdventDay3 {
-    
-    public static void main(String[] args) throws Exception {
+
+    public static void main(final String[] args) throws Exception {
         part1();
         part2();
     }
 
     private static void part1() throws IOException {
         try (BufferedReader in = readInput()) {
-            var memory = SpiralMemory.parseInput(in);
+            final var memory = SpiralMemory.parseInput(in);
             System.out.println("Part 1 - Answer %d".formatted(memory.findDistance()));
         }
     }
 
     private static void part2() throws IOException {
         try (BufferedReader in = readInput()) {
-            System.out.println("Part 2 - Answer %d".formatted(0));
+            final var memory = SpiralMemory.parseInput(in);
+            System.out.println("Part 2 - Answer %d".formatted(memory.findValue()));
         }
     }
 
--- day3/src/main/java/com/mscg/SpiralMemory.java
+++ day3/src/main/java/com/mscg/SpiralMemory.java
@@ -4,7 +4,10 @@ import io.soabase.recordbuilder.core.RecordBuilder;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public record SpiralMemory(int targetValue) {
 
@@ -16,37 +19,89 @@ public record SpiralMemory(int targetValue) {
         var position = new Position(level, Math.min(0, 1 - level));
         int value = (2 * (level - 1) + 1) * (2 * (level - 1) + 1) + 1;
 
-        enum Direction {
-            UP, LEFT, DOWN, RIGHT;
-        }
-
         var direction = Direction.UP;
 
         while (value < targetValue) {
             value++;
-            direction = switch (direction) {
-                case UP -> position.y() == level ? Direction.LEFT : Direction.UP;
-                case LEFT -> position.x() == -level ? Direction.DOWN : Direction.LEFT;
-                case DOWN -> position.y() == -level ? Direction.RIGHT : Direction.DOWN;
-                case RIGHT -> position.x() == level ? Direction.UP : Direction.RIGHT;
-            };
-            position = switch (direction) {
-                case UP -> position.with(p -> p.y(p.y() + 1));
-                case LEFT -> position.with(p -> p.x(p.x() - 1));
-                case DOWN -> position.with(p -> p.y(p.y() - 1));
-                case RIGHT -> position.with(p -> p.x(p.x() + 1));
-            };
+            direction = direction.next(position, level);
+            position = position.next(direction);
         }
 
         return position.x() + position.y();
+    }
+
+    public int findValue() {
+        final Map<Position, Integer> storedValues = new HashMap<>();
+        var position = new Position(0, 0);
+        int lastValue = 1;
+        int level = 0;
+        var direction = Direction.UP;
+        while (lastValue < targetValue) {
+            lastValue = Stream.of( //
+                    position.with(p -> p.x(p.x() + 1)), //
+                    position.with(p -> {
+                        p.x(p.x() + 1);
+                        p.y(p.y() + 1);
+                    }), //
+                    position.with(p -> p.y(p.y() + 1)), //
+                    position.with(p -> {
+                        p.x(p.x() - 1);
+                        p.y(p.y() + 1);
+                    }), //
+                    position.with(p -> p.x(p.x() - 1)), //
+                    position.with(p -> {
+                        p.x(p.x() - 1);
+                        p.y(p.y() - 1);
+                    }), //
+                    position.with(p -> p.y(p.y() - 1)), //
+                    position.with(p -> {
+                        p.x(p.x() + 1);
+                        p.y(p.y() - 1);
+                    })) //
+                    .mapToInt(p -> storedValues.getOrDefault(p, 0))
+                    .sum();
+            storedValues.put(position, Math.max(1, lastValue));
+            if (position.x() == level && position.y() == -level) {
+                // switch level
+                level++;
+                direction = Direction.UP;
+                position = position.with(p -> p.x(p.x() + 1));
+            } else {
+                direction = direction.next(position, level);
+                position = position.next(direction);
+            }
+        }
+        return lastValue;
     }
 
     public static SpiralMemory parseInput(final BufferedReader in) throws IOException {
         return new SpiralMemory(Integer.parseInt(in.readLine()));
     }
 
+    enum Direction {
+        UP, LEFT, DOWN, RIGHT;
+
+        public Direction next(final Position position, final int level) {
+            return switch (this) {
+                case UP -> position.y() == level ? Direction.LEFT : Direction.UP;
+                case LEFT -> position.x() == -level ? Direction.DOWN : Direction.LEFT;
+                case DOWN -> position.y() == -level ? Direction.RIGHT : Direction.DOWN;
+                case RIGHT -> position.x() == level ? Direction.UP : Direction.RIGHT;
+            };
+        }
+    }
+
     @RecordBuilder
     static record Position(int x, int y) implements SpiralMemoryPositionBuilder.With {
+
+        public Position next(final Direction direction) {
+            return switch (direction) {
+                case UP -> this.with(p -> p.y(p.y() + 1));
+                case LEFT -> this.with(p -> p.x(p.x() - 1));
+                case DOWN -> this.with(p -> p.y(p.y() - 1));
+                case RIGHT -> this.with(p -> p.x(p.x() + 1));
+            };
+        }
 
     }
 
