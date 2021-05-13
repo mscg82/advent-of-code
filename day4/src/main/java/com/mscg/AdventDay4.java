ed7f26ed3d5ee2adac118d498f7a20cfc2386246
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2021-05-13 19:38:43 +0200

    day 4 - part 1, 2

--- day4/src/main/java/com/mscg/AdventDay4.java
+++ day4/src/main/java/com/mscg/AdventDay4.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AdventDay4 {
 
@@ -14,13 +17,32 @@ public class AdventDay4 {
 
     private static void part1() throws IOException {
         try (BufferedReader in = readInput()) {
-            System.out.println("Part 1 - Answer %d".formatted(0));
+            final long validPassphrases = in.lines()
+                    .filter(line -> {
+                        final Map<String, Long> wordsCount = Arrays.stream(line.split(" ")) //
+                                .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+                        return wordsCount.values().stream().allMatch(l -> l == 1L);
+                    }) //
+                    .count();
+            System.out.println("Part 1 - Answer %d".formatted(validPassphrases));
         }
     }
 
     private static void part2() throws IOException {
         try (BufferedReader in = readInput()) {
-            System.out.println("Part 2 - Answer %d".formatted(0));
+            final long validPassphrases = in.lines()
+                    .filter(line -> {
+                        final Map<String, Long> wordsCount = Arrays.stream(line.split(" ")) //
+                                .map(s -> {
+                                    final byte[] bytes = s.getBytes();
+                                    Arrays.sort(bytes);
+                                    return new String(bytes);
+                                }) //
+                                .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+                        return wordsCount.values().stream().allMatch(l -> l == 1L);
+                    }) //
+                    .count();
+            System.out.println("Part 2 - Answer %d".formatted(validPassphrases));
         }
     }
 
