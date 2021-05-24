cde7d1b9ae49f2802d0b3e78d26ddffff28483f9
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-05-24 13:01:26 +0200

    day 7 - part 2

--- day7/src/main/java/com/mscg/ProcessTree.java
+++ day7/src/main/java/com/mscg/ProcessTree.java
@@ -4,10 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.LongSummaryStatistics;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import io.soabase.recordbuilder.core.RecordBuilder;
 
 public record ProcessTree(List<Process> processes) {
 
@@ -22,6 +28,52 @@ public record ProcessTree(List<Process> processes) {
                 .orElseThrow();
     }
 
+    public Process findBalancedProcess() {
+        final Map<String, Process> nameToProcess = processes.stream() //
+                .collect(Collectors.toMap(Process::name, p -> p));
+
+        final String baseProcess = findBaseProcess();
+
+        final Map<String, Long> processToTotalWeight = new HashMap<>();
+        computeWeight(nameToProcess.get(baseProcess), nameToProcess, processToTotalWeight);
+
+        final List<Process> unbalancedProcesses = processes.stream() //
+                .filter(p -> !p.subprocesses().isEmpty()) //
+                .filter(p -> {
+                    final long targetWeight = processToTotalWeight.get(p.subprocesses().get(0));
+                    return p.subprocesses().stream().anyMatch(sub -> processToTotalWeight.get(sub) != targetWeight);
+                }) //
+                .toList();
+
+        final Process unbalancedProcessParent = unbalancedProcesses.stream() //
+                .min(Comparator.comparingLong(p -> p.subprocesses().stream().mapToLong(processToTotalWeight::get).min().orElseThrow())) //
+                .orElseThrow();
+
+        final LongSummaryStatistics statistics = unbalancedProcessParent.subprocesses.stream() //
+                .mapToLong(processToTotalWeight::get) //
+                .summaryStatistics();
+        final int delta = (int) (statistics.getMax() - statistics.getMin());
+
+        final Process unbalancedProcess = unbalancedProcessParent.subprocesses().stream() //
+                .max(Comparator.comparingLong(processToTotalWeight::get)) //
+                .map(nameToProcess::get) //
+                .orElseThrow();
+
+        final var balancedProcess = unbalancedProcess.with(p -> p.weight(p.weight() - delta));
+        return balancedProcess;
+    }
+
+    private void computeWeight(final Process process, final Map<String, Process> nameToProcess, final Map<String, Long> processToTotalWeight) {
+        if (process.subprocesses().isEmpty()) {
+            processToTotalWeight.put(process.name(), (long) process.weight());
+        } else {
+            process.subprocesses().stream() //
+                    .map(nameToProcess::get) //
+                    .forEach(p -> computeWeight(p, nameToProcess, processToTotalWeight));
+            processToTotalWeight.put(process.name(), process.weight() + process.subprocesses().stream().mapToLong(processToTotalWeight::get).sum());
+        }
+    }
+
     public static ProcessTree parseInput(final BufferedReader in) throws IOException {
         try {
             final List<Process> processes = in.lines() //
@@ -34,7 +86,8 @@ public record ProcessTree(List<Process> processes) {
         }
     }
 
-    public static record Process(String name, int weight, List<String> subprocesses) {
+    @RecordBuilder
+    public static record Process(String name, int weight, List<String> subprocesses) implements ProcessTreeProcessBuilder.With {
 
         public static final Pattern FORMAT = Pattern.compile("^(\\w+) \\((\\d+)\\)( -> (.*))?$");
 
