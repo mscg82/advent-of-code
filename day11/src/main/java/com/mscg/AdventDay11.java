1eb3380667fde5d1605c45c6ebf2f8ea9bc578d5
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-06-13 20:04:21 +0200

    day 11 - part 1

--- day11/src/main/java/com/mscg/AdventDay11.java
+++ day11/src/main/java/com/mscg/AdventDay11.java
@@ -14,7 +14,8 @@ public class AdventDay11 {
 
     private static void part1() throws IOException {
         try (BufferedReader in = readInput()) {
-            System.out.println("Part 1 - Answer %d".formatted(0));
+            final var hex = HexMaze.parseInput(in, 2205);
+            System.out.println("Part 1 - Answer %d".formatted(hex.findDistance()));
         }
     }
 
---
+++ day11/src/main/java/com/mscg/HexMaze.java
@@ -0,0 +1,173 @@
+package com.mscg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+public final class HexMaze {
+
+    private final InitedTiles initedTiles;
+    private final List<Direction> directions;
+
+    public HexMaze(final int maxLayerWidth, final List<Direction> directions) {
+        this.initedTiles = initTiles(maxLayerWidth);
+        this.directions = directions;
+    }
+
+    public int findDistance() {
+        Tile targetTile = initedTiles.startingTile();
+        for (final var direction : directions) {
+            targetTile = targetTile.getNeighbours().get(direction);
+        }
+
+        final Stream<Tile> upperDescDiagonal = Stream.iterate(initedTiles.startingTile(), Objects::nonNull, t -> t.getNeighbours().get(Direction.NW));
+        final Stream<Tile> lowerDescDiagonal = Stream.iterate(initedTiles.startingTile(), Objects::nonNull, t -> t.getNeighbours().get(Direction.SE));
+        final Set<Integer> idsInDescDiagonal = Stream.concat(upperDescDiagonal, lowerDescDiagonal) //
+                .map(Tile::getId) //
+                .collect(Collectors.toUnmodifiableSet());
+
+        final Stream<Tile> upperAscDiagonal = Stream.iterate(initedTiles.startingTile(), Objects::nonNull, t -> t.getNeighbours().get(Direction.NE));
+        final Stream<Tile> lowerAscDiagonal = Stream.iterate(initedTiles.startingTile(), Objects::nonNull, t -> t.getNeighbours().get(Direction.SW));
+        final Set<Integer> idsInAscDiagonal = Stream.concat(upperAscDiagonal, lowerAscDiagonal) //
+                .map(Tile::getId) //
+                .collect(Collectors.toUnmodifiableSet());
+
+        Tile currentTile = targetTile;
+        int steps = 0;
+        while (!idsInDescDiagonal.contains(currentTile.getId()) && !idsInAscDiagonal.contains(currentTile.getId())) {
+            if (currentTile.getId() > initedTiles.startingTile().getId()) {
+                currentTile = currentTile.getNeighbours().get(Direction.S);
+            } else {
+                currentTile = currentTile.getNeighbours().get(Direction.N);
+            }
+            steps++;
+        }
+
+        if (idsInDescDiagonal.contains(currentTile.getId())) {
+            return steps + Math.abs(currentTile.getId() - initedTiles.startingTile().getId());
+        } else {
+            while (!currentTile.equals(initedTiles.startingTile())) {
+                if (currentTile.getId() > initedTiles.startingTile().getId()) {
+                    currentTile = currentTile.getNeighbours().get(Direction.SW);
+                } else {
+                    currentTile = currentTile.getNeighbours().get(Direction.NE);
+                }
+                steps++;
+            }
+            return steps;
+        }
+    }
+
+    private InitedTiles initTiles(final int maxLayerWidth) {
+        final Tile startingTile;
+        final List<Tile> allTiles = new ArrayList<>(maxLayerWidth * maxLayerWidth);
+
+        int id = 0;
+
+        List<Tile> previousLayer = List.of(new Tile(id++));
+        allTiles.addAll(previousLayer);
+
+        for (int width = 2; width <= maxLayerWidth; width++) {
+            final List<Tile> currentLayer = new ArrayList<>(width);
+            for (int i = 0; i < width; i++) {
+                final var tile = new Tile(id++);
+                allTiles.add(tile);
+                currentLayer.add(tile);
+                if (i != 0) {
+                    tile.connect(previousLayer.get(i - 1), Direction.SW);
+                    tile.connect(currentLayer.get(i - 1), Direction.NW);
+                }
+                if (i < previousLayer.size()) {
+                    tile.connect(previousLayer.get(i), Direction.S);
+                }
+            }
+            previousLayer = currentLayer;
+        }
+
+        startingTile = previousLayer.get(maxLayerWidth / 2);
+
+        for (int width = maxLayerWidth - 1; width >= 1; width--) {
+            final List<Tile> currentLayer = new ArrayList<>(width);
+            for (int i = 0; i < width; i++) {
+                final var tile = new Tile(id++);
+                allTiles.add(tile);
+                currentLayer.add(tile);
+                if (i != 0) {
+                    tile.connect(currentLayer.get(i - 1), Direction.NW);
+                }
+                tile.connect(previousLayer.get(i), Direction.SW);
+                tile.connect(previousLayer.get(i + 1), Direction.S);
+            }
+            previousLayer = currentLayer;
+        }
+
+        return new InitedTiles(startingTile, allTiles);
+    }
+
+    public static HexMaze parseInput(final BufferedReader in, final int maxLayerWidth) throws IOException {
+        final List<Direction> directions = Arrays.stream(in.readLine().split(",")) //
+                .map(Direction::from) //
+                .toList();
+        return new HexMaze(maxLayerWidth, directions);
+    }
+
+    public enum Direction {
+        N, NE, SE, S, SW, NW;
+
+        public Direction opposite() {
+            return switch (this) {
+                case N -> Direction.S;
+                case NE -> Direction.SW;
+                case SE -> Direction.NW;
+                case S -> Direction.N;
+                case SW -> Direction.NE;
+                case NW -> Direction.SE;
+            };
+        }
+
+        public static Direction from(final String value) {
+            return switch (value.toLowerCase()) {
+                case "n" -> N;
+                case "ne" -> NE;
+                case "se" -> SE;
+                case "s" -> S;
+                case "sw" -> SW;
+                case "nw" -> NW;
+                default -> throw new IllegalArgumentException("Invalid direction " + value);
+            };
+        }
+    }
+
+    @Getter
+    @EqualsAndHashCode(of = "id")
+    public static class Tile {
+        private final int id;
+        private final Map<Direction, Tile> neighbours;
+
+        public Tile(final int id) {
+            this.id = id;
+            this.neighbours = new EnumMap<>(Direction.class);
+        }
+
+        public void connect(final Tile other, final Direction direction) {
+            this.neighbours.put(direction, other);
+            other.neighbours.put(direction.opposite(), this);
+        }
+    }
+
+    private static record InitedTiles(Tile startingTile, List<Tile> allTiles) {
+
+    }
+
+}
