69deaee4ba6a8a34c77ca2cfd3ab9e01a1fa9ee9
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-06-30 11:40:30 +0200

    day 17 - part 1

---
+++ day17/src/main/java/com/mscg/CircularBuffer.java
@@ -0,0 +1,31 @@
+package com.mscg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public record CircularBuffer(int steps) {
+
+    public static CircularBuffer parseInput(final BufferedReader in) throws IOException {
+        return new CircularBuffer(Integer.parseInt(in.readLine()));
+    }
+
+    public int findShortcircuit() {
+        return findShortcircuit(2017);
+    }
+
+    public int findShortcircuit(final int repetitions) {
+        final ArrayList<Integer> buffer = new ArrayList<>();
+        buffer.add(0);
+
+        int position = 0;
+        for (int i = 1; i <= repetitions; i++) {
+            final int newPosition = (position + steps) % buffer.size();
+            buffer.add((newPosition + 1) % buffer.size(), i);
+            position = (newPosition + 1) % buffer.size();
+        }
+
+        return buffer.get((position + 1) % buffer.size());
+    }
+
+}
