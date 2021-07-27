683bee40e82abdd4660935dfa9b1accbfe8a578b
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-07-27 09:53:17 +0200

    Day 1 - part 1, 2

--- day1/src/main/java/com/mscg/AdventDay1.java
+++ day1/src/main/java/com/mscg/AdventDay1.java
@@ -4,23 +4,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AdventDay1 {
-    
-    public static void main(String[] args) throws Exception {
+
+    public static void main(final String[] args) throws Exception {
         part1();
         part2();
     }
 
     private static void part1() throws IOException {
         try (BufferedReader in = readInput()) {
-            System.out.println("Part 1 - Answer %d".formatted(0));
+            final long sum = in.lines() //
+                    .mapToLong(Long::parseLong) //
+                    .sum();
+            System.out.println("Part 1 - Answer %d".formatted(sum));
         }
     }
 
     private static void part2() throws IOException {
         try (BufferedReader in = readInput()) {
-            System.out.println("Part 2 - Answer %d".formatted(0));
+            final long[] values = in.lines() //
+                    .mapToLong(Long::parseLong) //
+                    .toArray();
+            final Set<Long> seenValues = new HashSet<>();
+            long value = 0L;
+            int idx = 0;
+            while (!seenValues.contains(value)) {
+                seenValues.add(value);
+                value += values[idx];
+                idx = (idx + 1) % values.length;
+            }
+            System.out.println("Part 2 - Answer %d".formatted(value));
         }
     }
 
