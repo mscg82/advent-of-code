ee7eb558bad182023a152ac0b1f34e2a959fc394
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2021-08-11 19:12:01 +0200

    day 4 - fixe test

--- day4/src/test/java/com/mscg/AdventDay4Test.java
+++ day4/src/test/java/com/mscg/AdventDay4Test.java
@@ -18,23 +18,23 @@ public class AdventDay4Test {
         try (var in = readInput()) {
             final var timeline = GuardTimeline.parseInput(in);
             Assertions.assertEquals(List.of( //
-                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 00), 10, TimelineAction.BEGIN_SHIFT),
-                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 05), 10, TimelineAction.FALLS_ASLEEP),
-                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 25), 10, TimelineAction.WAKES_UP),
-                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 30), 10, TimelineAction.FALLS_ASLEEP),
-                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 55), 10, TimelineAction.WAKES_UP),
-                    new TimelineEntry(new Timestamp(1518, 11, 01, 23, 58), 99, TimelineAction.BEGIN_SHIFT),
-                    new TimelineEntry(new Timestamp(1518, 11, 02, 00, 40), 99, TimelineAction.FALLS_ASLEEP),
-                    new TimelineEntry(new Timestamp(1518, 11, 02, 00, 50), 99, TimelineAction.WAKES_UP),
-                    new TimelineEntry(new Timestamp(1518, 11, 03, 00, 05), 10, TimelineAction.BEGIN_SHIFT),
-                    new TimelineEntry(new Timestamp(1518, 11, 03, 00, 24), 10, TimelineAction.FALLS_ASLEEP),
-                    new TimelineEntry(new Timestamp(1518, 11, 03, 00, 29), 10, TimelineAction.WAKES_UP),
-                    new TimelineEntry(new Timestamp(1518, 11, 04, 00, 02), 99, TimelineAction.BEGIN_SHIFT),
-                    new TimelineEntry(new Timestamp(1518, 11, 04, 00, 36), 99, TimelineAction.FALLS_ASLEEP),
-                    new TimelineEntry(new Timestamp(1518, 11, 04, 00, 46), 99, TimelineAction.WAKES_UP),
-                    new TimelineEntry(new Timestamp(1518, 11, 05, 00, 03), 99, TimelineAction.BEGIN_SHIFT),
-                    new TimelineEntry(new Timestamp(1518, 11, 05, 00, 45), 99, TimelineAction.FALLS_ASLEEP),
-                    new TimelineEntry(new Timestamp(1518, 11, 05, 00, 55), 99, TimelineAction.WAKES_UP)
+                    new TimelineEntry(new Timestamp(1518, 11, 1, 0, 0), 10, TimelineAction.BEGIN_SHIFT),
+                    new TimelineEntry(new Timestamp(1518, 11, 1, 0, 5), 10, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 1, 0, 25), 10, TimelineAction.WAKES_UP),
+                    new TimelineEntry(new Timestamp(1518, 11, 1, 0, 30), 10, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 1, 0, 55), 10, TimelineAction.WAKES_UP),
+                    new TimelineEntry(new Timestamp(1518, 11, 1, 23, 58), 99, TimelineAction.BEGIN_SHIFT),
+                    new TimelineEntry(new Timestamp(1518, 11, 2, 0, 40), 99, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 2, 0, 50), 99, TimelineAction.WAKES_UP),
+                    new TimelineEntry(new Timestamp(1518, 11, 3, 0, 5), 10, TimelineAction.BEGIN_SHIFT),
+                    new TimelineEntry(new Timestamp(1518, 11, 3, 0, 24), 10, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 3, 0, 29), 10, TimelineAction.WAKES_UP),
+                    new TimelineEntry(new Timestamp(1518, 11, 4, 0, 2), 99, TimelineAction.BEGIN_SHIFT),
+                    new TimelineEntry(new Timestamp(1518, 11, 4, 0, 36), 99, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 4, 0, 46), 99, TimelineAction.WAKES_UP),
+                    new TimelineEntry(new Timestamp(1518, 11, 5, 0, 3), 99, TimelineAction.BEGIN_SHIFT),
+                    new TimelineEntry(new Timestamp(1518, 11, 5, 0, 45), 99, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 5, 0, 55), 99, TimelineAction.WAKES_UP)
             ), timeline.entries());
         }
     }
