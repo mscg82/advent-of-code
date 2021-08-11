f084f6c1fbdf16e6d3da6e48d75a848517a46cb6
Giuseppe Miscione <mscg.giuseppe@gmail.com>
2021-08-11 17:18:13 +0200

    Day 4 - tmp

--- day4/src/main/java/com/mscg/GuardTimeline.java
+++ day4/src/main/java/com/mscg/GuardTimeline.java
@@ -1,12 +1,75 @@
 package com.mscg;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
 
-public class GuardTimeline {
+public record GuardTimeline(List<TimelineEntry> entries) {
 
-    public record Timestamp(int year, int month, int day, int hour, int min) implements Comparable<Timestamp> {
+    public static GuardTimeline parseInput(final BufferedReader in) throws IOException {
+        try {
+            final var pattern = Pattern.compile("\\[(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2})] (.+)");
+            final List<TimelineEntry> timelineEntries = in.lines() //
+                    .map(pattern::matcher) //
+                    .filter(Matcher::matches) //
+                    .map(matcher -> {
+                        final var timestamp = new Timestamp( //
+                                Integer.parseInt(matcher.group(1)), //
+                                Integer.parseInt(matcher.group(2)), //
+                                Integer.parseInt(matcher.group(3)), //
+                                Integer.parseInt(matcher.group(4)), //
+                                Integer.parseInt(matcher.group(5)) //
+                        );
+
+                        final String actionStr = matcher.group(6);
+                        final int guard;
+                        final TimelineAction action;
+                        if (actionStr.startsWith("Guard #")) {
+                            final int index = actionStr.indexOf('#') + 1;
+                            final int endIndex = actionStr.indexOf(' ', index);
+                            guard = Integer.parseInt(actionStr, index, endIndex, 10);
+                            action = TimelineAction.BEGIN_SHIFT;
+                        } else if ("wakes up".equals(actionStr)) {
+                            guard = 0;
+                            action = TimelineAction.WAKES_UP;
+                        } else if ("falls asleep".equals(actionStr)) {
+                            guard = 0;
+                            action = TimelineAction.FALLS_ASLEEP;
+                        } else {
+                            throw new IllegalArgumentException("Unable to parse action string " + actionStr);
+                        }
+
+                        return new TimelineEntry(timestamp, guard, action);
+                    }) //
+                    .sorted(Comparator.comparing(TimelineEntry::time)) //
+                    .toList();
+
+            final var latestEncounteredGuardId = new AtomicInteger(0);
+            final List<TimelineEntry> fixedTimelineEntries = timelineEntries.stream() //
+                    .map(entry -> switch (entry.action()) {
+                        case BEGIN_SHIFT -> {
+                            latestEncounteredGuardId.set(entry.guard());
+                            yield entry;
+                        }
+                        case WAKES_UP, FALLS_ASLEEP -> entry.withGuard(latestEncounteredGuardId.intValue());
+                    }) //
+                    .toList();
+
+            return new GuardTimeline(fixedTimelineEntries);
+        }
+        catch (final UncheckedIOException e) {
+            throw e.getCause();
+        }
+    }
+
+    public static record Timestamp(int year, int month, int day, int hour, int min) implements Comparable<Timestamp> {
 
         private static final Comparator<Timestamp> COMPARATOR = Comparator //
                 .comparingInt(Timestamp::year) //
@@ -26,7 +89,7 @@ public class GuardTimeline {
     }
 
     @RecordBuilder
-    public record TimelineEntry(Timestamp time, int guard, TimelineAction action) {
+    public static record TimelineEntry(Timestamp time, int guard, TimelineAction action) implements GuardTimelineTimelineEntryBuilder.With {
 
     }
 }
--- day4/src/test/java/com/mscg/AdventDay4Test.java
+++ day4/src/test/java/com/mscg/AdventDay4Test.java
@@ -1,2 +1,42 @@
-package com.mscg;public class AdventDay4Test {
+package com.mscg;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import com.mscg.GuardTimeline.TimelineAction;
+import com.mscg.GuardTimeline.TimelineEntry;
+import com.mscg.GuardTimeline.Timestamp;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AdventDay4Test {
+
+    @Test
+    public void testParse() throws Exception {
+        try (var in = readInput()) {
+            var timeline = GuardTimeline.parseInput(in);
+            Assertions.assertEquals(List.of( //
+                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 00), 10, TimelineAction.BEGIN_SHIFT),
+                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 05), 10, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 25), 10, TimelineAction.WAKES_UP),
+                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 30), 10, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 01, 00, 55), 10, TimelineAction.WAKES_UP),
+                    new TimelineEntry(new Timestamp(1518, 11, 01, 23, 58), 99, TimelineAction.BEGIN_SHIFT),
+                    new TimelineEntry(new Timestamp(1518, 11, 01, 23, 58), 99, TimelineAction.BEGIN_SHIFT),
+                    new TimelineEntry(new Timestamp(1518, 11, 02, 00, 40), 99, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 02, 00, 50), 99, TimelineAction.WAKES_UP)
+            new TimelineEntry(new Timestamp(1518, 11, 03, 00, 05), 10, TimelineAction.BEGIN_SHIFT),
+                    new TimelineEntry(new Timestamp(1518, 11, 03, 00, 24), 10, TimelineAction.FALLS_ASLEEP),
+                    new TimelineEntry(new Timestamp(1518, 11, 03, 00, 29), 10, TimelineAction.WAKES_UP),
+            ),timeline.entries());
+        }
+    }
+
+    private BufferedReader readInput() {
+        return new BufferedReader(
+                new InputStreamReader(this.getClass().getResourceAsStream("/test-input.txt"), StandardCharsets.UTF_8));
+    }
+
 }
