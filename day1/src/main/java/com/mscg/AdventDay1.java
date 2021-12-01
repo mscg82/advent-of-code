072913499eb2f1b965bc112727558258df0f3bf5
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2021-12-01 11:46:39 +0100

    Day 1 - part 1, 2

--- day1/src/main/java/com/mscg/AdventDay1.java
+++ day1/src/main/java/com/mscg/AdventDay1.java
@@ -1,12 +1,15 @@
 package com.mscg;
 
+import org.jooq.lambda.Seq;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
 public class AdventDay1 {
-    
+
     public static void main(String[] args) throws Exception {
         part1();
         part2();
@@ -14,14 +17,37 @@ public class AdventDay1 {
 
     private static void part1() throws IOException {
         try (BufferedReader in = readInput()) {
-            System.out.println("Part 1 - Answer %d".formatted(0));
+            final Seq<Integer> readings = Seq.seq(in.lines()) //
+                    .map(Integer::parseInt);
+            final long incrementsCount = getIncrementsCount(readings);
+
+            System.out.println("Part 1 - Answer %d".formatted(incrementsCount));
         }
     }
 
     private static void part2() throws IOException {
         try (BufferedReader in = readInput()) {
-            System.out.println("Part 2 - Answer %d".formatted(0));
+            final Seq<Integer> windows3 = Seq.seq(in.lines()) //
+                    .map(Integer::parseInt) //
+                    .sliding(3) //
+                    .map(AdventDay1::sumValuesInWindow);
+
+            final long windowsIncrementsCount = getIncrementsCount(windows3);
+
+            System.out.println("Part 2 - Answer %d".formatted(windowsIncrementsCount));
         }
+    }
+
+    private static long getIncrementsCount(Seq<Integer> inputSequence) {
+        return inputSequence //
+                .sliding(2) //
+                .map(Stream::toList) //
+                .filter(w -> w.get(1) > w.get(0)) //
+                .count();
+    }
+
+    private static Integer sumValuesInWindow(Seq<Integer> s) {
+        return s.foldLeft(0, Integer::sum);
     }
 
     private static BufferedReader readInput() {
