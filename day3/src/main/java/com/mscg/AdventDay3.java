0818b37dfa123f4332e457f1069bd8731d508cb7
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2021-12-07 23:00:10 +0100

    Day 3 - part 2

--- day3/src/main/java/com/mscg/AdventDay3.java
+++ day3/src/main/java/com/mscg/AdventDay3.java
@@ -22,7 +22,8 @@ public class AdventDay3 {
 
     private static void part2() throws IOException {
         try (BufferedReader in = readInput()) {
-            System.out.println("Part 2 - Answer %d".formatted(0));
+            final var wireGrid = WireGrid.parseInput(in);
+            System.out.println("Part 2 - Answer %d".formatted(wireGrid.findFastestIntersectionLength()));
         }
     }
 
--- day3/src/main/java/com/mscg/WireGrid.java
+++ day3/src/main/java/com/mscg/WireGrid.java
@@ -2,11 +2,15 @@ package com.mscg;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Set;
+import java.util.function.Supplier;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
@@ -22,29 +26,48 @@ public record WireGrid(List<Instruction> wire1, List<Instruction> wire2) {
         return new WireGrid(wire1, wire2);
     }
 
-    public Position findClosestIntersection() {
-        final Set<Position> wire1Pos = new HashSet<>();
+    private static <C extends Collection<Position>> C getWirePositions(final List<Instruction> wire, final Supplier<C> collectionSupplier,
+                                                                       final PositionFilter positionsFilter) {
+        final C wire1Pos = collectionSupplier.get();
         Position lastPosition = Position.CENTRAL_PORT;
-        for (final Instruction instr : wire1) {
+        for (final ListIterator<Instruction> iterator = wire.listIterator(); iterator.hasNext(); ) {
+            final int index = iterator.nextIndex();
+            final Instruction instr = iterator.next();
             final List<Position> newPositions = instr.generatePositions(lastPosition);
-            wire1Pos.addAll(newPositions);
+            wire1Pos.addAll(positionsFilter.accept(index, newPositions));
             lastPosition = newPositions.get(newPositions.size() - 1);
         }
+        return wire1Pos;
+    }
+
+    public Position findClosestIntersection() {
+        final Set<Position> wire1Pos = getWirePositions(wire1, HashSet::new, PositionFilter.ALL);
         wire1Pos.remove(Position.CENTRAL_PORT);
 
-        final Set<Position> wire2Pos = new HashSet<>();
-        lastPosition = Position.CENTRAL_PORT;
-        for (final Instruction instr : wire2) {
-            final List<Position> newPositions = instr.generatePositions(lastPosition);
-            wire2Pos.addAll(newPositions);
-            lastPosition = newPositions.get(newPositions.size() - 1);
-        }
+        final Set<Position> wire2Pos = getWirePositions(wire2, HashSet::new, PositionFilter.ALL);
         wire2Pos.remove(Position.CENTRAL_PORT);
 
         wire1Pos.retainAll(wire2Pos);
 
         return wire1Pos.stream() //
                 .min(Comparator.comparingLong(Position::distance)) //
+                .orElseThrow();
+    }
+
+    public int findFastestIntersectionLength() {
+        final PositionFilter positionFilter = (idx, positions) -> idx == 0 ? positions : positions.subList(1, positions.size());
+
+        final List<Position> wire1Pos = getWirePositions(wire1, ArrayList::new, positionFilter);
+
+        final List<Position> wire2Pos = getWirePositions(wire2, ArrayList::new, positionFilter);
+
+        final Set<Position> intersections = getWirePositions(wire1, HashSet::new, PositionFilter.ALL);
+        intersections.retainAll(getWirePositions(wire2, HashSet::new, PositionFilter.ALL));
+        intersections.remove(Position.CENTRAL_PORT);
+
+        return intersections.stream() //
+                .mapToInt(intersection -> wire1Pos.indexOf(intersection) + wire2Pos.indexOf(intersection)) //
+                .min() //
                 .orElseThrow();
     }
 
@@ -60,6 +83,15 @@ public record WireGrid(List<Instruction> wire1, List<Instruction> wire2) {
                 default -> throw new IllegalArgumentException("Unsupported direction " + c);
             };
         }
+    }
+
+    @FunctionalInterface
+    private interface PositionFilter {
+
+        PositionFilter ALL = (__, positions) -> positions;
+
+        List<Position> accept(int index, List<Position> positions);
+
     }
 
     public record Instruction(Direction direction, long amount) {
