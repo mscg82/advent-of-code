477cea94e88bd83f6e3f8843e2774e3375928f30
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2021-12-11 16:52:37 +0100

    Day 9 - part 1, 2

--- day9/src/main/java/com/mscg/AdventDay9.java
+++ day9/src/main/java/com/mscg/AdventDay9.java
@@ -4,29 +4,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 
-public class AdventDay9 {
+public class AdventDay9
+{
 
-    public static void main(final String[] args) throws Exception {
-        part1();
-        part2();
-    }
+	public static void main(final String[] args) throws Exception
+	{
+		part1();
+		part2();
+	}
 
-    private static void part1() throws IOException {
-        try (BufferedReader in = readInput()) {
-            System.out.println("Part 1 - Answer %d".formatted(0));
-        }
-    }
+	private static void part1() throws IOException
+	{
+		try (BufferedReader in = readInput()) {
+			final var computer = IntcodeV4.parseInput(in);
+			final IntcodeV4 executed = computer.execute(List.of(1L).iterator(), false);
+			System.out.println("Part 1 - Answer %s".formatted(Arrays.toString(executed.outputs())));
+		}
+	}
 
-    private static void part2() throws IOException {
-        try (BufferedReader in = readInput()) {
-            System.out.println("Part 2 - Answer %d".formatted(0));
-        }
-    }
+	private static void part2() throws IOException
+	{
+		try (BufferedReader in = readInput()) {
+			final var computer = IntcodeV4.parseInput(in);
+			final IntcodeV4 executed = computer.execute(List.of(2L).iterator(), false);
+			System.out.println("Part 2 - Answer %s".formatted(Arrays.toString(executed.outputs())));
+		}
+	}
 
-    private static BufferedReader readInput() {
-        return new BufferedReader(
-                new InputStreamReader(AdventDay9.class.getResourceAsStream("/input.txt"), StandardCharsets.UTF_8));
-    }
+	private static BufferedReader readInput()
+	{
+		return new BufferedReader(
+				new InputStreamReader(AdventDay9.class.getResourceAsStream("/input.txt"), StandardCharsets.UTF_8));
+	}
 
 }
