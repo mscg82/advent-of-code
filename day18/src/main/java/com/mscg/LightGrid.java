08e76f567684ce51f9efb9bb6f65f6736b2d1233
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2022-07-03 17:51:05 +0200

    Updated to java 17

--- day18/src/main/java/com/mscg/LightGrid.java
+++ day18/src/main/java/com/mscg/LightGrid.java
@@ -1,111 +1,116 @@
 package com.mscg;
 
+import lombok.Getter;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import lombok.Getter;
-
 @Getter
-public class LightGrid {
+public class LightGrid
+{
 
-    private final List<List<Light>> lights;
-    private final boolean cornersStuck;
+	private final List<List<Light>> lights;
 
-    public LightGrid(final List<List<Light>> lights) {
-        this(lights, false);
-    }
+	private final boolean cornersStuck;
 
-    public LightGrid(final List<List<Light>> lights, boolean cornersStuck) {
-        if (cornersStuck) {
-            List<Light> row = lights.get(0);
-            row.set(0, Light.ON);
-            row.set(row.size() - 1, Light.ON);
+	public static LightGrid parseInput(final BufferedReader in, final boolean cornerStuck) throws IOException
+	{
+		final List<? extends List<Light>> lights = in.lines() //
+				.map(line -> line.chars() //
+						.mapToObj(c -> Light.of((char) c)) //
+						.collect(Collectors.toCollection(ArrayList::new))) //
+				.toList();
+		return new LightGrid(lights, cornerStuck);
+	}
 
-            row = lights.get(lights.size() - 1);
-            row.set(0, Light.ON);
-            row.set(row.size() - 1, Light.ON);
-        }
-        this.cornersStuck = cornersStuck;
-        this.lights = lights.stream() //
-                .map(List::copyOf) //
-                .collect(Collectors.toUnmodifiableList());
-    }
+	public LightGrid(final List<List<Light>> lights)
+	{
+		this(lights, false);
+	}
 
-    public LightGrid next() {
-        List<List<Light>> newLights = cloneLights();
-        for (int i = 0, l = lights.size(); i < l; i++) {
-            List<Light> row = lights.get(i);
-            for (int j = 0, l2 = row.size(); j < l2; j++) {
-                List<Light> neighbours = new ArrayList<>(8);
-                for (int k1 = Math.max(0, i - 1); k1 <= Math.min(l - 1, i + 1); k1++) {
-                    for (int k2 = Math.max(0, j - 1); k2 <= Math.min(l2 - 1, j + 1); k2++) {
-                        if (k1 != i || k2 != j) {
-                            neighbours.add(lights.get(k1).get(k2));
-                        }
-                    }
-                }
-                int onNeighbours = (int) neighbours.stream() //
-                        .filter(light -> light == Light.ON) //
-                        .count();
-                newLights.get(i).set(j, switch (lights.get(i).get(j)) {
-                    case ON -> switch (onNeighbours) {
-                            case 2, 3 -> Light.ON;
-                            default -> Light.OFF;
-                        };
-                    case OFF -> switch (onNeighbours) {
-                            case 3 -> Light.ON;
-                            default -> Light.OFF;
-                        };
-                });
-            }
-        }
+	public LightGrid(final List<? extends List<Light>> lights, final boolean cornersStuck)
+	{
+		if (cornersStuck) {
+			List<Light> row = lights.get(0);
+			row.set(0, Light.ON);
+			row.set(row.size() - 1, Light.ON);
 
-        return new LightGrid(newLights, cornersStuck);
-    }
+			row = lights.get(lights.size() - 1);
+			row.set(0, Light.ON);
+			row.set(row.size() - 1, Light.ON);
+		}
+		this.cornersStuck = cornersStuck;
+		this.lights = lights.stream() //
+				.map(List::copyOf) //
+				.toList();
+	}
 
-    @Override
-    public String toString() {
-        return lights.stream() //
-                .map(row -> row.stream().map(Object::toString).collect(Collectors.joining())) //
-                .collect(Collectors.joining("\n"));
-    }
+	public LightGrid next()
+	{
+		final var newLights = cloneLights();
+		for (int i = 0, l = lights.size(); i < l; i++) {
+			final List<Light> row = lights.get(i);
+			for (int j = 0, l2 = row.size(); j < l2; j++) {
+				final List<Light> neighbours = new ArrayList<>(8);
+				for (int k1 = Math.max(0, i - 1); k1 <= Math.min(l - 1, i + 1); k1++) {
+					for (int k2 = Math.max(0, j - 1); k2 <= Math.min(l2 - 1, j + 1); k2++) {
+						if (k1 != i || k2 != j) {
+							neighbours.add(lights.get(k1).get(k2));
+						}
+					}
+				}
+				final int onNeighbours = (int) neighbours.stream() //
+						.filter(light -> light == Light.ON) //
+						.count();
+				newLights.get(i).set(j, switch (lights.get(i).get(j)) {
+					case ON -> (onNeighbours == 2 || onNeighbours == 3) ? Light.ON : Light.OFF;
+					case OFF -> (onNeighbours == 3) ? Light.ON : Light.OFF;
+				});
+			}
+		}
 
-    private List<List<Light>> cloneLights() {
-        return lights.stream() //
-                .map(ArrayList::new) //
-                .collect(Collectors.toList());
-    }
+		return new LightGrid(newLights, cornersStuck);
+	}
 
-    public static LightGrid parseInput(BufferedReader in, boolean cornerStuck) throws IOException {
-        List<List<Light>> lights = in.lines() //
-                .map(line -> line.chars() //
-                        .mapToObj(c -> Light.of((char) c)) //
-                        .collect(Collectors.toList())) //
-                .collect(Collectors.toList());
-        return new LightGrid(lights, cornerStuck);
-    }
+	@Override
+	public String toString()
+	{
+		return lights.stream() //
+				.map(row -> row.stream().map(Object::toString).collect(Collectors.joining())) //
+				.collect(Collectors.joining("\n"));
+	}
 
-    public enum Light {
-        ON, OFF;
+	private List<? extends List<Light>> cloneLights()
+	{
+		return lights.stream() //
+				.map(ArrayList::new) //
+				.toList();
+	}
 
-        @Override
-        public String toString() {
-            return switch (this) {
-                case ON -> "#";
-                case OFF -> ".";
-            };
-        }
+	public enum Light
+	{
+		ON, OFF;
 
-        public static Light of(char c) {
-            return switch (c) {
-                case '#' -> Light.ON;
-                case '.' -> Light.OFF;
-                default -> throw new IllegalArgumentException("Invalid light state char " + c);
-            };
-        }
-    }
+		public static Light of(final char c)
+		{
+			return switch (c) {
+				case '#' -> Light.ON;
+				case '.' -> Light.OFF;
+				default -> throw new IllegalArgumentException("Invalid light state char " + c);
+			};
+		}
+
+		@Override
+		public String toString()
+		{
+			return switch (this) {
+				case ON -> "#";
+				case OFF -> ".";
+			};
+		}
+	}
 
 }
