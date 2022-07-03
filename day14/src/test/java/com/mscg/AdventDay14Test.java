323cad83db4ec133965fb31e7ed25fb28e99ea25
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2022-07-03 17:58:05 +0200

    Updated to java 17

--- day14/src/test/java/com/mscg/AdventDay14Test.java
+++ day14/src/test/java/com/mscg/AdventDay14Test.java
@@ -7,56 +7,63 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class AdventDay14Test {
+class AdventDay14Test
+{
 
-    @Test
-    public void testFirstIndex() throws Exception {
-        try (BufferedReader in = readInput()) {
-            final var padGenerator = OnePadGenerator.parseInput(in);
-            final long index = padGenerator.findNthPad(1, false);
-            Assertions.assertEquals(39, index);
-        }
-    }
+	@Test
+	void testFirstIndex() throws Exception
+	{
+		try (BufferedReader in = readInput()) {
+			final var padGenerator = OnePadGenerator.parseInput(in);
+			final long index = padGenerator.findNthPad(1, false);
+			Assertions.assertEquals(39, index);
+		}
+	}
 
-    @Test
-    public void testSecondIndex() throws Exception {
-        try (BufferedReader in = readInput()) {
-            final var padGenerator = OnePadGenerator.parseInput(in);
-            final long index = padGenerator.findNthPad(2, false);
-            Assertions.assertEquals(92, index);
-        }
-    }
+	@Test
+	void testSecondIndex() throws Exception
+	{
+		try (BufferedReader in = readInput()) {
+			final var padGenerator = OnePadGenerator.parseInput(in);
+			final long index = padGenerator.findNthPad(2, false);
+			Assertions.assertEquals(92, index);
+		}
+	}
 
-    // @Test
-    public void test64thIndex() throws Exception {
-        try (BufferedReader in = readInput()) {
-            final var padGenerator = OnePadGenerator.parseInput(in);
-            final long index = padGenerator.findNthPad(64, false);
-            Assertions.assertEquals(22728, index);
-        }
-    }
+	// @Test
+	void test64thIndex() throws Exception
+	{
+		try (BufferedReader in = readInput()) {
+			final var padGenerator = OnePadGenerator.parseInput(in);
+			final long index = padGenerator.findNthPad(64, false);
+			Assertions.assertEquals(22728, index);
+		}
+	}
 
-    @Test
-    public void testFirstIndexStretched() throws Exception {
-        try (BufferedReader in = readInput()) {
-            final var padGenerator = OnePadGenerator.parseInput(in);
-            final long index = padGenerator.findNthPad(1, true);
-            Assertions.assertEquals(10, index);
-        }
-    }
+	@Test
+	void testFirstIndexStretched() throws Exception
+	{
+		try (BufferedReader in = readInput()) {
+			final var padGenerator = OnePadGenerator.parseInput(in);
+			final long index = padGenerator.findNthPad(1, true);
+			Assertions.assertEquals(10, index);
+		}
+	}
 
-    // @Test
-    public void test64thIndexStretched() throws Exception {
-        try (BufferedReader in = readInput()) {
-            final var padGenerator = OnePadGenerator.parseInput(in);
-            final long index = padGenerator.findNthPad(64, true);
-            Assertions.assertEquals(22551, index);
-        }
-    }
+	// @Test
+	void test64thIndexStretched() throws Exception
+	{
+		try (BufferedReader in = readInput()) {
+			final var padGenerator = OnePadGenerator.parseInput(in);
+			final long index = padGenerator.findNthPad(64, true);
+			Assertions.assertEquals(22551, index);
+		}
+	}
 
-    private BufferedReader readInput() {
-        return new BufferedReader(
-                new InputStreamReader(this.getClass().getResourceAsStream("/test-input.txt"), StandardCharsets.UTF_8));
-    }
+	private BufferedReader readInput()
+	{
+		return new BufferedReader(
+				new InputStreamReader(this.getClass().getResourceAsStream("/test-input.txt"), StandardCharsets.UTF_8));
+	}
 
 }
