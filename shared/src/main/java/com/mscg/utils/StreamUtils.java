590f9524488f7cf9b1fb551b820b354e208af5be
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2022-12-03 20:39:59 +0100

    Added utility methods for streams

--- shared/src/main/java/com/mscg/utils/StreamUtils.java
+++ shared/src/main/java/com/mscg/utils/StreamUtils.java
@@ -1,9 +1,14 @@
 package com.mscg.utils;
 
+import com.mscg.utils.spliterators.PartitionedSpliterator;
+import com.mscg.utils.spliterators.WindowedSpliterator;
 import lombok.NonNull;
 
+import java.util.Collection;
+import java.util.Spliterator;
 import java.util.function.BinaryOperator;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public final class StreamUtils
 {
@@ -19,8 +24,31 @@ public final class StreamUtils
 		};
 	}
 
+	public static <T> @NonNull Stream<Collection<T>> windowed(@NonNull final Collection<T> source, final int size)
+	{
+		return windowed(source.stream(), size);
+	}
+
+	public static <T> @NonNull Stream<Collection<T>> windowed(@NonNull final Stream<T> stream, final int size)
+	{
+		final var windower = new WindowedSpliterator<>(stream.spliterator(), size);
+		return StreamSupport.stream(windower, false);
+	}
+
+	public static <T> @NonNull Stream<Collection<T>> partitioned(@NonNull final Collection<T> source, final int size)
+	{
+		return partitioned(source.stream(), size);
+	}
+
+	public static <T> @NonNull Stream<Collection<T>> partitioned(@NonNull final Stream<T> stream, final int size)
+	{
+		final Spliterator<Collection<T>> partitioner = new PartitionedSpliterator<>(stream.spliterator(), size);
+		return StreamSupport.stream(partitioner, false);
+	}
+
 	private StreamUtils()
 	{
 		throw new UnsupportedOperationException("StreamUtils constructor can't be called");
 	}
+
 }
---
+++ shared/src/main/java/com/mscg/utils/spliterators/WindowedSpliterator.java
@@ -0,0 +1,55 @@
+package com.mscg.utils.spliterators;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class WindowedSpliterator<T> implements Spliterator<Collection<T>>
+{
+	private final Deque<T> window;
+
+	private final Spliterator<T> source;
+
+	private final int size;
+
+	public WindowedSpliterator(final Spliterator<T> source, final int size)
+	{
+		this.window = new ArrayDeque<>(size);
+		this.source = source;
+		this.size = size;
+	}
+
+	@Override
+	public boolean tryAdvance(final Consumer<? super Collection<T>> action)
+	{
+		if (source.tryAdvance(window::addLast)) {
+			if (window.size() == size) {
+				action.accept(Collections.unmodifiableCollection(window));
+				window.removeFirst();
+			}
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public Spliterator<Collection<T>> trySplit()
+	{
+		return null;
+	}
+
+	@Override
+	public long estimateSize()
+	{
+		return Long.MAX_VALUE;
+	}
+
+	@Override
+	public int characteristics()
+	{
+		return 0;
+	}
+}
