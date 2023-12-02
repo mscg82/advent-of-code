29fd4c2e2a46bc035098bad7eba0cf1f79d8bc58
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2023-12-02 19:30:52 +0100

    Day 2 - part 1, 2

--- day2/src/main/java/com/mscg/AdventDay2.java
+++ day2/src/main/java/com/mscg/AdventDay2.java
@@ -17,14 +17,16 @@ public class AdventDay2
 	private static void part1() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println(STR."Part 1 - Answer \{0}");
+			final var game = CubeGame.parseInput(in);
+			System.out.println(STR."Part 1 - Answer \{game.computePossibleGames()}");
 		}
 	}
 
 	private static void part2() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println(STR."Part 2 - Answer \{0}");
+			final var game = CubeGame.parseInput(in);
+			System.out.println(STR."Part 2 - Answer \{game.computeMinPowerSum()}");
 		}
 	}
 
