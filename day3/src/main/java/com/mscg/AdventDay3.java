aa22ce4a808b5699ebf41e8cca4456204bf440d0
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2023-12-03 09:25:06 +0100

    Day 3 - part 1, 2

--- day3/src/main/java/com/mscg/AdventDay3.java
+++ day3/src/main/java/com/mscg/AdventDay3.java
@@ -18,14 +18,16 @@ public class AdventDay3
 	private static void part1() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println("Part 1 - Answer %d".formatted(0));
+			final var schematic = EngineSchematic.parseInput(in);
+			System.out.println("Part 1 - Answer %d".formatted(schematic.sumPartNumbers()));
 		}
 	}
 
 	private static void part2() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println("Part 2 - Answer %d".formatted(0));
+			final var schematic = EngineSchematic.parseInput(in);
+			System.out.println("Part 2 - Answer %d".formatted(schematic.sumGearRatios()));
 		}
 	}
 
