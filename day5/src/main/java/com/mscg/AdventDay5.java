081457397e70d5ebaf42ceb89326cf086b932032
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2023-12-05 10:15:01 +0100

    Day 5 - part 1, 2

--- day5/src/main/java/com/mscg/AdventDay5.java
+++ day5/src/main/java/com/mscg/AdventDay5.java
@@ -17,14 +17,16 @@ public class AdventDay5
 	private static void part1() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println(STR."Part 1 - Answer \{0}");
+			final var almanac = Almanac.parseInput(in);
+			System.out.println(STR."Part 1 - Answer \{almanac.findMinLocation()}");
 		}
 	}
 
 	private static void part2() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println(STR."Part 2 - Answer \{0}");
+			final var almanac = Almanac.parseInput(in);
+			System.out.println(STR."Part 2 - Answer \{almanac.findMinLocationByRanges()}");
 		}
 	}
 
