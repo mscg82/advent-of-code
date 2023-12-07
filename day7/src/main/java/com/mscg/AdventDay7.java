323cfcb66b776d190ffca19b9a6f10302e09c848
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2023-12-07 12:10:33 +0100

    Day 7 - part 1

--- day7/src/main/java/com/mscg/AdventDay7.java
+++ day7/src/main/java/com/mscg/AdventDay7.java
@@ -18,7 +18,8 @@ public class AdventDay7
 	private static void part1() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println("Part 1 - Answer %d".formatted(0));
+			final var cards = CamelCards.parseInput(in);
+			System.out.println("Part 1 - Answer %d".formatted(cards.computeTotalWinning()));
 		}
 	}
 
