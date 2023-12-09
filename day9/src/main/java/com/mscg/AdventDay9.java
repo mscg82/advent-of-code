a97ac0883244eccdec8d24234f4671fca237e8af
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2023-12-09 08:15:53 +0100

    Day 9 - part 1, 2

--- day9/src/main/java/com/mscg/AdventDay9.java
+++ day9/src/main/java/com/mscg/AdventDay9.java
@@ -18,14 +18,16 @@ public class AdventDay9
 	private static void part1() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println("Part 1 - Answer %d".formatted(0));
+			final var oasisAnalyzer = OasisAnalyzer.parseInput(in);
+			System.out.println("Part 1 - Answer %d".formatted(oasisAnalyzer.sumExtrapolatedNextValues()));
 		}
 	}
 
 	private static void part2() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println("Part 2 - Answer %d".formatted(0));
+			final var oasisAnalyzer = OasisAnalyzer.parseInput(in);
+			System.out.println("Part 2 - Answer %d".formatted(oasisAnalyzer.sumExtrapolatedPreviousValues()));
 		}
 	}
 
