f2f5335426436efe69353417de0d945603a650e8
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2023-12-10 22:32:46 +0100

    Day 10 - part 2

--- day10/src/main/java/com/mscg/AdventDay10.java
+++ day10/src/main/java/com/mscg/AdventDay10.java
@@ -25,7 +25,8 @@ public class AdventDay10
 	private static void part2() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println(STR."Part 2 - Answer \{0}");
+			final var pipesMap = PipesMap.parseInput(in);
+			System.out.println(STR."Part 2 - Answer \{pipesMap.computeAreaInsideLoop()}");
 		}
 	}
 
