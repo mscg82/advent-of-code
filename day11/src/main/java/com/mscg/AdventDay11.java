a1d68f0c9492281b4eef7f09e0f3669a32523f64
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2023-12-11 10:39:30 +0100

    Day 11 - part 2

--- day11/src/main/java/com/mscg/AdventDay11.java
+++ day11/src/main/java/com/mscg/AdventDay11.java
@@ -26,7 +26,8 @@ public class AdventDay11
 	private static void part2() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println("Part 2 - Answer %d".formatted(0));
+			final var skyMap = SkyMap.parseInput(in);
+			System.out.println("Part 2 - Answer %d".formatted(skyMap.sumDistancesBetweenGalaxiesInOlderUniverse()));
 		}
 	}
 
