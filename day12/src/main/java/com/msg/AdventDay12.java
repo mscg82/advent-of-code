ce16686b6e3fea1b84f4d2bd2d17888b4a0ff08f
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2023-12-13 11:36:45 +0100

    Day 12 - part 1

--- day12/src/main/java/com/msg/AdventDay12.java
+++ day12/src/main/java/com/msg/AdventDay12.java
@@ -18,7 +18,8 @@ public class AdventDay12
 	private static void part1() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println("Part 1 - Answer %d".formatted(0));
+			final var springField = SpringField.parseInput(in);
+			System.out.println("Part 1 - Answer %d".formatted(springField.sumAllArrangements()));
 		}
 	}
 
