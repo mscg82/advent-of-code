6a922463dd726f6fef09951010cdde22a9a0637b
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2024-12-03 11:08:52 +0100

    Day 3 - part 1, 2

--- day3/src/main/java/com/mscg/AdventDay3.java
+++ day3/src/main/java/com/mscg/AdventDay3.java
@@ -18,14 +18,16 @@ public class AdventDay3
 	private static void part1() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println("Part 1 - Answer %d".formatted(0));
+			final var computer = ShopComputer.parseInput(in);
+			System.out.println("Part 1 - Answer %d".formatted(computer.sumMultiplicationResults()));
 		}
 	}
 
 	private static void part2() throws IOException
 	{
 		try (BufferedReader in = readInput()) {
-			System.out.println("Part 2 - Answer %d".formatted(0));
+			final var computer = ShopComputer.parseInput(in);
+			System.out.println("Part 2 - Answer %d".formatted(computer.sumEnabledMultiplicationResults()));
 		}
 	}
 
