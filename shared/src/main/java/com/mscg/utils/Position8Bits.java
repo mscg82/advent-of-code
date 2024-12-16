2f7875846bc1717d97ecc6ea0f9eebebf1438efc
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2024-12-16 16:50:33 +0100

    Added utility class to handle positions with an optimized hash code

---
+++ shared/src/main/java/com/mscg/utils/Position8Bits.java
@@ -0,0 +1,36 @@
+package com.mscg.utils;
+
+import io.soabase.recordbuilder.core.RecordBuilderFull;
+
+@RecordBuilderFull
+public record Position8Bits(int x, int y) implements Position8BitsBuilder.With
+{
+
+	// Custom equals and hashCode are needed to optimize the performances
+	@Override
+	public boolean equals(final Object obj)
+	{
+		if (!(obj instanceof final Position8Bits other)) {
+			return false;
+		}
+		return hashCode() == other.hashCode();
+	}
+
+	@Override
+	public int hashCode()
+	{
+		if (x < 0 || x >= 256) {
+			throw new IllegalArgumentException("x out of range: " + x);
+		}
+		if (y < 0 || y >= 256) {
+			throw new IllegalArgumentException("y out of range: " + y);
+		}
+		return y << 8 | x;
+	}
+
+	public boolean isValid(final int rows, final int cols)
+	{
+		return x >= 0 && x < rows && y >= 0 && y < cols;
+	}
+
+}
